b3851062e3898880c173baad3de7954ef6dfad44
Sara Binazir <sarabinazir@gmail.com>
2019-12-11 14:39:44 +0100

    Park Controller&View

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Garage2.Models;
 
 namespace Garage2.Controllers
 {
-    public class VehiclesController
+    public class VehiclesController:Controller
     {
+        
+        // Get: Vehicle/Park
+        public IActionResult Park()
+        {
+            return View();
+
+        }
+
+        // Post: Vehicle/Park
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Park([Bind("RegistrationNumber,Type,Colour,Manufacturer,Model,NumberOfWheels")] ParkedVehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(vehicle);
+
+        }
+
 
 
 
