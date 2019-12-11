f00775a3ee96b0d21528593502d20cbbd7fb46e0
Rocangus <hunter_sniper112@hotmail.com>
2019-12-11 15:41:40 +0100

    Update VehicleSummaryViewModel

--- ViewModels/VehicleSummaryViewModel.cs
+++ ViewModels/VehicleSummaryViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Garage2.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,11 @@ namespace Garage2.ViewModels
 {
     public class VehicleSummaryViewModel
     {
+        public string RegistrationNumber { get; set; }
 
+        public VehicleType Type { get; set; }
+
+        public string Colour { get; set; }
 
     }
 }
