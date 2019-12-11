6e0a6331979d871baeda8d2c1121af9f0e3e5b8d
Rocangus <hunter_sniper112@hotmail.com>
2019-12-11 15:34:32 +0100

    Add ParkedVehicleViewModel

---
+++ ViewModels/ParkedVehicleVewModel.cs
@@ -0,0 +1,36 @@
+﻿using Garage2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2.ViewModels
+{
+    public class ParkedVehicleVewModel
+    {
+        public class ParkedVehicle
+        {
+            [Key]
+            [RegularExpression(@"^([A-Z]|[a-z]){3}\d{2,3}([A-Z]|[a-z]){0,1}")]
+            public string RegistrationNumber { get; set; }
+
+            [Required]
+            public VehicleType Type { get; set; }
+
+            [MaxLength(15)]
+            public string Colour { get; set; }
+
+            [MaxLength(30)]
+            public string Manufacturer { get; set; }
+
+            [MaxLength(60)]
+            public string Model { get; set; }
+
+            [Range(2, 18)]
+            public int NumberOfWheels { get; set; }
+
+            public DateTime ParkingDate { get; set; }
+        }
+    }
+}
