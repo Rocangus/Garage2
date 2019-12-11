caee584df8b51a42eddfadfb0a9d382ed6240d23
Rocangus <hunter_sniper112@hotmail.com>
2019-12-11 15:00:37 +0100

    Various fixes including ensuring necessary packages in Garage2.csproj are included.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -5,12 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Garage2.Models;
+using Garage2.Data;
 
 namespace Garage2.Controllers
 {
     public class VehiclesController:Controller
     {
-        
+        private GarageContext _context;
+
+        public VehiclesController(GarageContext context)
+        {
+            _context = context;
+        }
+
         // Get: Vehicle/Park
         public IActionResult Park()
         {
