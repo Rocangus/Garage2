6c7564d5b343df1562659252c386e6763ad12a6a
Rocangus <hunter_sniper112@hotmail.com>
2019-12-12 09:18:12 +0100

    Added single initial data seed entry

--- Data/GarageContext.cs
+++ Data/GarageContext.cs
@@ -14,6 +14,19 @@ namespace Garage2.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ParkedVehicle pay276 = new ParkedVehicle { RegistrationNumber = "PAY276", Colour = "Red", Manufacturer = "Skoda", Model = "Fabia Combi 1.2 TSI", NumberOfWheels = 4, Type = VehicleType.Car };
+            modelBuilder.Entity<ParkedVehicle>()
+                .HasData(
+                    pay276
+                );
+            modelBuilder.Entity<ParkingContract>()
+                .HasData(
+                    new { Id=1, ParkingDate=DateTime.Parse("2019-08-30"), VehicleRegistrationNumber="PAY276"}
+                );
+        }
+
         public DbSet<ParkedVehicle> ParkedVehicles { get; set; }
         public DbSet<ParkingContract> Contracts { get; set; }
     }
---
+++ Migrations/20191212081019_AddBaseData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage2.Migrations
+{
+    public partial class AddBaseData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ParkedVehicles",
+                columns: new[] { "RegistrationNumber", "Colour", "Manufacturer", "Model", "NumberOfWheels", "Type" },
+                values: new object[] { "PAY276", "Red", "Skoda", "Fabia Combi 1.2 TSI", 4, 0 });
+
+            migrationBuilder.InsertData(
+                table: "Contracts",
+                columns: new[] { "Id", "ParkingDate", "VehicleRegistrationNumber" },
+                values: new object[] { 1, new DateTime(2019, 8, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), "PAY276" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Contracts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicles",
+                keyColumn: "RegistrationNumber",
+                keyValue: "PAY276");
+        }
+    }
+}
