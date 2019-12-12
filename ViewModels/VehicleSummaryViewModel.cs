8961500264c3b9f5473cc2935b09d83435a169c1
Rocangus <hunter_sniper112@hotmail.com>
2019-12-12 09:30:12 +0100

    Add ParkingTime (currently date when parked) to VehicleSummaryViewModel

--- ViewModels/VehicleSummaryViewModel.cs
+++ ViewModels/VehicleSummaryViewModel.cs
@@ -15,5 +15,8 @@ namespace Garage2.ViewModels
 
         public string Colour { get; set; }
 
+        // Currently just the date the vehicle was parked
+        public DateTime ParkingTime { get; set; }
+
     }
 }
