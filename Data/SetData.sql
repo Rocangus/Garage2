cd21b19a4b502aab0b1ebc3f7a4d194b01b4e180
Rocangus <hunter_sniper112@hotmail.com>
2019-12-27 11:01:12 +0100

    Remove inserts into Contracts from SetData.sql

--- Data/SetData.sql
+++ Data/SetData.sql
@@ -8,15 +8,4 @@ INSERT INTO GarageContext.[dbo].[ParkedVehicles] ([RegistrationNumber], [Type], 
 INSERT INTO GarageContext.[dbo].[ParkedVehicles] ([RegistrationNumber], [Type], [Colour], [Manufacturer], [Model], [NumberOfWheels], [ParkingDate]) VALUES (N'PAY276', 0, N'Red', N'Skoda', N'Fabia Combi 1.2 TSI', 4, N'2019-12-17 10:15:34')
 INSERT INTO GarageContext.[dbo].[ParkedVehicles] ([RegistrationNumber], [Type], [Colour], [Manufacturer], [Model], [NumberOfWheels], [ParkingDate]) VALUES (N'PSA321', 1, N'White', N'BMW', N'RS1000RR', 2, N'2019-12-17 10:16:37')
 INSERT INTO GarageContext.[dbo].[ParkedVehicles] ([RegistrationNumber], [Type], [Colour], [Manufacturer], [Model], [NumberOfWheels], [ParkingDate]) VALUES (N'REO32L', 1, N'Orange', N'Honda', N'Super Cub', 2, N'2019-12-17 11:29:25')
-INSERT INTO GarageContext.[dbo].[ParkedVehicles] ([RegistrationNumber], [Type], [Colour], [Manufacturer], [Model], [NumberOfWheels], [ParkingDate]) VALUES (N'WQR43A', 1, N'Yellow', N'Honda', N'CRF 1000L AFRICA TWIN RALLY', 2, N'2019-12-17 11:42:22')
-
-SET IDENTITY_INSERT GarageContext.[dbo].[Contracts] ON
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (1, N'PAY276', N'2019-12-13 10:35:26')
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (2, N'AAA123', N'2019-12-13 08:29:47')
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (7, N'GEX033', N'2019-12-16 11:10:41')
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (18, N'HUJ341', N'2019-12-17 09:25:12')
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (19, N'AHI742', N'2019-12-17 10:15:34')
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (20, N'REO32L', N'2019-12-17 10:16:37')
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (21, N'PSA321', N'2019-12-17 11:29:25')
-INSERT INTO GarageContext.[dbo].[Contracts] ([Id], [VehicleRegistrationNumber], [ParkingDate]) VALUES (22, N'WQR43A', N'2019-12-17 11:42:22')
-SET IDENTITY_INSERT GarageContext.[dbo].[Contracts] OFF
+INSERT INTO GarageContext.[dbo].[ParkedVehicles] ([RegistrationNumber], [Type], [Colour], [Manufacturer], [Model], [NumberOfWheels], [ParkingDate]) VALUES (N'WQR43A', 1, N'Yellow', N'Honda', N'CRF 1000L AFRICA TWIN RALLY', 2, N'2019-12-17 11:42:22');
