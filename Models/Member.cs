bc9c637bbcc40ec413e3498c514a06ba31210ccf
Rocangus <hunter_sniper112@hotmail.com>
2019-12-27 10:48:24 +0100

    Add Member class

---
+++ Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CityAddress { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<ParkedVehicle> OwnedVehicles { get; set; }
+    }
+}
