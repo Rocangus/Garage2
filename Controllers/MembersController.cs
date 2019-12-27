cd991538dd76b751dae64bf12dbf70f650344374
Sara Binazir <sarabinazir@gmail.com>
2019-12-27 13:40:23 +0100

    Create MemberSummaryViewModel

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,31 @@
+﻿using Garage2.Data;
+using Garage2.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2.Controllers
+{
+    public class MembersController: Controller
+    {
+
+        private GarageContext _context;
+        public MembersController(GarageContext context)
+        {
+            _context = context;
+            
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var model = new MemberSummaryViewModel();
+
+            
+
+            return View();
+        }
+    }
+}
---
+++ ViewModels/MemberSummaryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2.ViewModels
+{
+    public class MemberSummaryViewModel
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Count { get; set; }
+
+    }
+}
