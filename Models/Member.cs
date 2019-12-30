edca9a6f187041a61759669ee80999cda3ed07cd
Rocangus <hunter_sniper112@hotmail.com>
2019-12-30 14:41:09 +0100

    Add FullName to Member

--- Models/Member.cs
+++ Models/Member.cs
@@ -7,6 +7,7 @@ namespace Garage2.Models
         public int MemberId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get => FirstName + " " + LastName; }
         public string CityAddress { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
